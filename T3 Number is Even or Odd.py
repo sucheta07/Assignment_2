03dc91ea959a8d9a55d07db00c8252c6f58d05f6
sucheta07 <86112442+sucheta07@users.noreply.github.com>
2025-04-07 11:59:12 +0530

    Add files via upload

---
+++ T3 Number is Even or Odd.py
@@ -0,0 +1,8 @@
+# Entry the input
+a = int(input("Enter the number : "))
+
+#Condition for even or odd
+if (a%2 == 0) :
+    print(a,"is an Even Number.")
+else :
+    print(a,"is an Odd Number.")
---
+++ T4 Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,4 @@
+sum = 0
+for a in range(1, 51):
+    sum = sum + a
+print("The sum of numbers from 1 to",a,"is:",sum)
